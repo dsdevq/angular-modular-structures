0f181adaf1f8a95b5336cf5cfe4f402b25e79c83
Denys <payar3282@gmail.com>
2024-09-30 20:57:48 +0300

    ddd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AuthComponent } from './features/auth/auth.component';
+import { DashboardComponent } from './features/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule
